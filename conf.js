1ecc352c184de3fbd2aca1713f3e6b14aabd73e3
Sunny Singh <sunnysingh2821@gmail.com>
2023-10-20 20:44:46 +0530

    chromedriver changes

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     chromeDriver: '/usr/local/share/chromedriver-linux64/chromedriver',
     specs: ['spec.js'],
-    directConnect: false,
+    directConnect: true,
     multiCapabilities: [{
         browserName: 'chrome',
         chromeOptions: {
