f7f2d0c6e9eaabe349cf02fa986234a26a8cebb4
Sunny Singh <sunnysingh2821@gmail.com>
2023-10-20 20:22:39 +0530

    chromedriver changes

--- conf.js
+++ conf.js
@@ -1,6 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
     specs: ['spec.js'],
     directConnect: false,
     multiCapabilities: [{
